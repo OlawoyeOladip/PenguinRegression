9d8aabaa830751d17fab6b6f468a4268617c752f
OlawoyeOladip <oladipupoolawoye26@gmail.com>
2025-08-16 16:14:46 +0100

    .

--- src/viz.py
+++ src/viz.py
@@ -7,10 +7,10 @@ def app():
 
     with st.expander("ℹ️ Take a quick glance at the data"):
         st.write("Here’s the raw penguins dataset preview.")
-        st.write(pd.read_csv(r"data\penguins.csv"))
+        st.write(pd.read_csv(r"src/data/penguins.csv"))
 
     # Base directory for images
-    BASE_DIR = Path(r"images")
+    BASE_DIR = Path(r"src/images")
 
     # List PNG/JPG files in the folder
     image_files = sorted([f for f in BASE_DIR.iterdir() if f.suffix.lower() in [".png", ".jpg", ".jpeg"]])
